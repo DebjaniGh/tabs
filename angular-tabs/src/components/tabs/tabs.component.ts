5d2027e33a46750727f621ac525a09ec6ec94e38
Ghosh, Debjani <Debjani.Ghosh@dell.com>
2025-10-07 15:36:26 +0530

    Tabs implementation in Angular

---
+++ angular-tabs/src/components/tabs/tabs.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+export interface tabsConfig {
+  id: number;
+  title: string;
+  content: string;
+  contentColor: string;
+}
+
+@Component({
+  selector: 'app-tabs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tabs.component.html',
+  styleUrl: './tabs.component.css',
+})
+export class TabsComponent {
+  activeTabId: number = 1;
+
+  tabs: tabsConfig[] = [
+    {
+      id: 1,
+      title: 'Tab 1',
+      content: 'Content for Tab 1',
+      contentColor: '#f0f2e9',
+    },
+    {
+      id: 2,
+      title: 'Tab 2',
+      content: 'Content for Tab 2',
+      contentColor: '#f2fff5',
+    },
+    {
+      id: 3,
+      title: 'Tab 3',
+      content: 'Content for Tab 3',
+      contentColor: '#e8e9ed',
+    },
+  ];
+
+  selectTab(id: number) {
+    this.activeTabId = id;
+  }
+}
